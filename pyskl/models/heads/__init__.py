11e31c258ec6b998a7172f906dc35d916e7041ae
jlgzb <gzbcumtb@163.com>
2022-07-30 21:36:08 +0800

    upload GTCNHead

--- pyskl/models/heads/__init__.py
+++ pyskl/models/heads/__init__.py
@@ -2,6 +2,6 @@
 # flake8: noqa: F401
 from .base import BaseHead, BaseHeadCom
 from .simple_head import GCNHead, I3DHead, SimpleHead, SlowFastHead, TSNHead
-from .simple_head import GCNHeadCom, SimpleHeadCom
+from .simple_head import GCNHeadCom, SimpleHeadCom, SimpleHeadGTCN
 
 from .cigcn_head import GTCNHeadCoco
--- pyskl/models/heads/simple_head.py
+++ pyskl/models/heads/simple_head.py
@@ -6,6 +6,11 @@ from ..builder import HEADS
 from .base import BaseHead, BaseHeadCom
 
 
+# by gzb: for parallel gtcn
+from ...utils import Graph
+from ..gcns import GCNBlock, CNNBlock
+from ..gcns import unit_tcn2
+
 @HEADS.register_module()
 class SimpleHead(BaseHead):
     """ A simple classification head.
@@ -36,7 +41,7 @@ class SimpleHead(BaseHead):
             self.dropout = nn.Dropout(p=self.dropout_ratio)
         else:
             self.dropout = None
-        assert mode in ['3D', 'GCN', '2D']
+        assert mode in ['3D', 'GCN', '2D', 'GTCN']
         self.mode = mode
 
         self.in_c = in_channels
@@ -284,4 +289,132 @@ class GCNHeadCom(SimpleHeadCom):
                          dropout=dropout,
                          init_std=init_std,
                          mode='GCN',
+                         **kwargs)
+
+
+@HEADS.register_module()
+class SimpleHeadGTCN(BaseHead):
+    """ A simple classification head.
+
+    Args:
+        num_classes (int): Number of classes to be classified.
+        in_channels (int): Number of channels in input feature.
+        loss_cls (dict): Config for building loss. Default: dict(type='CrossEntropyLoss')
+        dropout (float): Probability of dropout layer. Default: 0.5.
+        init_std (float): Std value for Initiation. Default: 0.01.
+        kwargs (dict, optional): Any keyword argument to be used to initialize
+            the head.
+    """
+
+    def __init__(self,
+                 num_classes,
+                 in_channels,
+                 graph_cfg=dict(layout='coco', mode='spatial'),
+                 loss_cls=dict(type='CrossEntropyLoss'),
+                 dropout=0.5,
+                 init_std=0.01,
+                 mode='3D',
+                 **kwargs):
+        super().__init__(num_classes, in_channels, loss_cls, **kwargs)
+
+        self.dropout_ratio = dropout
+        self.init_std = init_std
+        if self.dropout_ratio != 0:
+            self.dropout = nn.Dropout(p=self.dropout_ratio)
+        else:
+            self.dropout = None
+        assert mode in ['3D', 'GCN', '2D', 'GTCN']
+        self.mode = mode
+
+        # by gzb: for parallel TCN GCN
+        self.graph = Graph(**graph_cfg)
+        A = torch.tensor(self.graph.A, dtype=torch.float32, requires_grad=False)
+        self.para1_tcn = CNNBlock(in_channels=256, out_channels=256, dilations=[1,2])
+        self.para2_gcn = GCNBlock(in_channels=256, out_channels=256, A=A.clone(), adaptive=True)
+        self.uni_tcn = unit_tcn2(256 * 2, 256 * 2, seg=25, mode='TV')
+
+        self.in_c = in_channels
+        self.fc_cls = nn.Linear(self.in_c, num_classes)
+
+    def init_weights(self):
+        """Initiate the parameters from scratch."""
+        normal_init(self.fc_cls, std=self.init_std)
+
+    def forward(self, x):
+        """Defines the computation performed at every call.
+
+        Args:
+            x (torch.Tensor): The input data.
+
+        Returns:
+            torch.Tensor: The classification scores for input samples.
+        """
+
+        if isinstance(x, list):
+            for item in x:
+                assert len(item.shape) == 2
+            x = [item.mean(dim=0) for item in x]
+            x = torch.stack(x)
+
+        if len(x.shape) != 2:
+            if self.mode == '2D':
+                assert len(x.shape) == 5
+                N, S, C, H, W = x.shape
+                pool = nn.AdaptiveAvgPool2d(1)
+                x = x.reshape(N * S, C, H, W)
+                x = pool(x)
+                x = x.reshape(N, S, C)
+                x = x.mean(dim=1)
+            if self.mode == '3D':
+                pool = nn.AdaptiveAvgPool3d(1)
+                if isinstance(x, tuple) or isinstance(x, list):
+                    x = torch.cat(x, dim=1)
+                x = pool(x)
+                x = x.view(x.shape[:2])
+            if self.mode == 'GCN':
+                pool = nn.AdaptiveAvgPool2d(1)
+                N, M, C, T, V = x.shape
+                x = x.reshape(N * M, C, T, V) # by gzb: N*M 256 100/4 17
+
+                x = pool(x) # by gzb: N*M 256 1 1
+                x = x.reshape(N, M, C) # by gzb: N M 256
+                x = x.mean(dim=1) # by gzb: N 256
+                
+            if self.mode == 'GTCN':
+                pool = nn.AdaptiveMaxPool2d(1)
+                N, M, C, T, V = x.shape
+                x = x.reshape(N * M, C, T, V) # by gzb: N*M 256 100/4 17
+
+                x_tcn = self.para1_tcn(x) # -1 256 25 17
+                x_gcn = self.para2_gcn(x) # -1 256 25 17
+                x = torch.concat([x_tcn, x_gcn], dim=1) # -1 512 25 17
+                x = self.uni_tcn(x) # -1 512 25 1
+
+                x = pool(x) # by gzb: N*M 256 1 1
+                x = x.reshape(N, M, C) # by gzb: N M 256
+                x = x.mean(dim=1) # by gzb: N 256
+
+        assert x.shape[1] == self.in_c
+        if self.dropout is not None:
+            x = self.dropout(x)
+
+        cls_score = self.fc_cls(x)
+        return cls_score
+
+@HEADS.register_module()
+class GTCNHead(SimpleHeadGTCN):
+
+    def __init__(self,
+                 num_classes,
+                 in_channels,
+                 loss_cls=dict(type='CrossEntropyLoss'),
+                 dropout=0.,
+                 init_std=0.01,
+                 **kwargs):
+        super().__init__(num_classes,
+                         in_channels,
+                         loss_cls=loss_cls,
+                         dropout=dropout,
+                         init_std=init_std,
+                         mode='GTCN',
                          **kwargs)
